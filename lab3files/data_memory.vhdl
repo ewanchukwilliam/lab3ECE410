fb47af0249435cb74361e7c736867c121c06aca1
williamewanchuk <williamewanchuk@compliantai.com>
2025-11-21 11:43:04 -0700

    cleaning repo

--- lab3files/data_memory.vhdl
+++ lab3files/data_memory.vhdl
@@ -50,19 +50,23 @@ ARCHITECTURE rtl OF data_mem IS
 
     TYPE memory_data IS ARRAY (0 TO 1023) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
     SIGNAL RAM : memory_data := (
-        -- BEQ TEST DATA (little-endian)
-        -- Address 0: 0x00000005 (value for x1)
-        0 => x"05", 1 => x"00", 2 => x"00", 3 => x"00",
-        -- Address 4: 0x00000005 (value for x2, same as x1 -> branch taken)
-        4 => x"05", 5 => x"00", 6 => x"00", 7 => x"00",
-        -- Address 8: 0x00000063 (99 for x3, should be skipped)
-        8 => x"63", 9 => x"00", 10 => x"00", 11 => x"00",
-        -- Address 12: 0x0000000A (10 for x4)
-        12 => x"0A", 13 => x"00", 14 => x"00", 15 => x"00",
-        -- Address 16: 0x00000007 (7 for x5, different from x4 -> branch not taken)
-        16 => x"07", 17 => x"00", 18 => x"00", 19 => x"00",
-        -- Address 20: 0x00000064 (100 for x6)
-        20 => x"64", 21 => x"00", 22 => x"00", 23 => x"00",
+        -- Custom Test Data (little-endian)
+        -- Address 0x0: 0x0AAAAAAA
+        0 => x"AA", 1 => x"AA", 2 => x"AA", 3 => x"0A",
+        -- Address 0x4: 0x00000024
+        4 => x"24", 5 => x"00", 6 => x"00", 7 => x"00",
+        -- Address 0x8: 0x00000043
+        8 => x"43", 9 => x"00", 10 => x"00", 11 => x"00",
+        -- Address 0xC: 0xBABECAFE
+        12 => x"FE", 13 => x"CA", 14 => x"BE", 15 => x"BA",
+        -- Address 0x10: 0x00000002
+        16 => x"02", 17 => x"00", 18 => x"00", 19 => x"00",
+        -- Address 0x14: 0xFFFFFFFF
+        20 => x"FF", 21 => x"FF", 22 => x"FF", 23 => x"FF",
+        -- Address 0x18: 0x00000000
+        24 => x"00", 25 => x"00", 26 => x"00", 27 => x"00",
+        -- Address 0x1C: 0xA3700A37
+        28 => x"37", 29 => x"0A", 30 => x"70", 31 => x"A3",
         OTHERS => (OTHERS => '0')
     );
 
